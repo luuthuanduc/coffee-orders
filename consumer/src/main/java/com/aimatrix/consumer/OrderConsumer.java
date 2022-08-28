52b779391af74bcf349f3caa169acff1bbb91b10
Duc Luu <ducluuthuan@gmail.com>
2022-08-28 20:47:07 +0700

    Using specific.protobuf.value.type

--- consumer/src/main/java/com/aimatrix/consumer/OrderConsumer.java
+++ consumer/src/main/java/com/aimatrix/consumer/OrderConsumer.java
@@ -1,8 +1,6 @@
 package com.aimatrix.consumer;
 
 import com.aimatrix.domain.generated.CoffeeOrder;
-import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,14 +11,9 @@ import org.springframework.stereotype.Component;
 public class OrderConsumer {
 
     @KafkaListener(topics = {"${spring.kafka.template.default-topic}"})
-    public void onMessage(ConsumerRecord<String, DynamicMessage> consumerRecord) {
-        try {
-            CoffeeOrder coffeeOrder =
-                    CoffeeOrder.newBuilder().build().getParserForType().parseFrom(consumerRecord.value().toByteArray());
-            log.info("ConsumerRecord key: {} , value: {}", coffeeOrder.getId(), coffeeOrder);
-        } catch (InvalidProtocolBufferException exception) {
-            exception.printStackTrace();
-        }
+    public void onMessage(ConsumerRecord<String, CoffeeOrder> consumerRecord) {
+        CoffeeOrder coffeeOrder = consumerRecord.value();
+        log.info("CoffeeOrder key: {} , value: {}", coffeeOrder.getId(), coffeeOrder);
     }
 
 }
